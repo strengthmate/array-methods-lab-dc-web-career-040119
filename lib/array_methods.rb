77342f82ebe0dd58001b3c108343dda0ded973c4
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 18:06:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,17 +9,17 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-  array.rever
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  
 end
