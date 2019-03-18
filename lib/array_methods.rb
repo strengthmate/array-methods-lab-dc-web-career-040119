0d23a39006885894b34f7d3c101c5d5d7d4a03d2
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 18:05:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,15 @@
 def using_include(array, element)
-
+  if array.include element
+    puts true
+  else puts false
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.rever
 end
 
 def using_first(array)
